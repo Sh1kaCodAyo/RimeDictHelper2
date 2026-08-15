927704853b64e4e15d051dda0670e52527fe1849
zjh <zhoujia983@126.com>
2026-08-15 21:39:21 +0800

    调整目录结构

--- src/GlobalVariables.h
+++ src/GlobalVariables.h
@@ -32,17 +32,3 @@ extern std::unordered_map<std::wstring, std::wstring> g_charCodeMap;
 extern WCHAR szTitle[MAX_LOADSTRING];
 extern WCHAR szWindowClass[MAX_LOADSTRING];
 extern WNDPROC g_oldWordProc, g_oldCodeProc, g_oldWeightProc;
-
-// const wchar_t* CONFIG_FILE = L".\\config.ini";
-// const wchar_t* CONFIG_SECTION = L"Settings";
-// const wchar_t* CONFIG_KEY_BASE_DICT = L"BaseDictPath";
-// const wchar_t* CONFIG_KEY_USER_DICT = L"UserDictPath";
-// const wchar_t* SCRIPT_NAME = L".\\after.bat";
-// bool enableSync = TRUE;
-// HFONT g_hFont;
-// HINSTANCE hInst;
-// HWND hWord, hCode, hWeight, hParent, hStatusBar, hBtnAdd, hBtnSync, hBtnAddSync, g_hWnd;
-// std::unordered_map<std::wstring, std::wstring> g_charCodeMap;
-// WCHAR szTitle[MAX_LOADSTRING];
-// WCHAR szWindowClass[MAX_LOADSTRING];
-// WNDPROC g_oldWordProc = nullptr, g_oldCodeProc = nullptr, g_oldWeightProc = nullptr;
